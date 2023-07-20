ee1ab7dfcc6f9d311ca6b48aab5141b5148786b7
Lex <aowusuopare@gmail.com>
2023-07-20 15:33:23 -0700

    done done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,8 +12,8 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 5 == 0) && (i % 3 == 0))
-                {
-                        printf("FizzBuzz");
+		{
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
